176aad6e6db33a99f925029c69a6f88370acf038
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2023-01-11 11:15:23 +0100

    fix query default

--- src/Routes/Read.php
+++ src/Routes/Read.php
@@ -17,10 +17,11 @@ class Read implements IRoute{
             $db->direct('SET SESSION group_concat_max_len = 4294967295;');
             try{
                 $_REQUEST['tablename']=$tablename;
+                $_REQUEST['replace']=1;
                 $db->direct('call dsx_rest_api_get({request},@result);',['request'=>json_encode($_REQUEST)]);
                 $o = json_decode($db->singleValue('select @result res',[],'res'),true);
                 App::result('data',$o['data']);
-                App::result('count',count($o['count']));
+                App::result('count',$o['count']);
                 App::result('success', $o['success']);
             }catch(\Exception $e){
                 App::result('last_sql', $db->last_sql );
