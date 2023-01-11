0d23f86c5881cf96d3a5add14668ea0a3ead45b2
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2023-01-11 11:26:03 +0100

    add debug query

--- src/Routes/Read.php
+++ src/Routes/Read.php
@@ -20,8 +20,9 @@ class Read implements IRoute{
                 if (!isset($_REQUEST['count'])) $_REQUEST['count']=1;
 
                 $db->direct('call dsx_rest_api_get({request},@result);',['request'=>json_encode($_REQUEST)]);
+
                 $o = json_decode($db->singleValue('select @result res',[],'res'),true);
-                App::result('data',$o['data']);
+                if (isset($o['debug_query'])) App::result('data',$o['debug_query']);
                 App::result('total',$o['total']);
                 App::result('success', $o['success']);
             }catch(\Exception $e){
