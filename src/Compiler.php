4b2d807c4719ea58eb01d916ebf7e18e1ed332a9
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2023-02-24 10:57:28 +0100

    append stores

--- src/Compiler.php
+++ src/Compiler.php
@@ -24,6 +24,7 @@ class Compiler implements ICompiler {
             if(($matches['file']=='all') || ($matches['file']=='form')) $data = array_merge($data,$db->direct('select js,table_name,"view_ds_form" m from view_ds_form limit 2000 '));
             if(($matches['file']=='all') || ($matches['file']=='dsview')) $data = array_merge($data,$db->direct('select js,table_name,"view_ds_dsview" m from view_ds_dsview limit 2000  '));
             */     
+
             file_put_contents(
                 TualoApplication::get('tempPath').'/models.js',
                 array_reduce(
@@ -36,7 +37,26 @@ class Compiler implements ICompiler {
             );
 
             $files[] = [
-                'toolkit'=>'classic',
+                'toolkit'=>'',
+                'modul'=>'dsx',
+                'files'=>TualoApplication::get('tempPath').'/jscache/model.js'
+            ];
+
+
+
+            file_put_contents(
+                TualoApplication::get('tempPath').'/stores.js',
+                array_reduce(
+                    $db->direct('select js,table_name,"view_ds_store" m from view_ds_store' ), 
+                    function($acc,$item){
+                        return $acc."\n".
+                            "/* console.debug('".$item['table_name']."','".$item['m']."');*/".
+                            "\n".$item['js'];
+                })
+            );
+
+            $files[] = [
+                'toolkit'=>'',
                 'modul'=>'dsx',
                 'files'=>TualoApplication::get('tempPath').'/jscache/model.js'
             ];
